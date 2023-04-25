5c19f7f7c1f6bfd0a029039fcc499787b57b257b
elj40 <elaijoubert@gmail.com>
2023-04-25 18:49:15 +0200

    Detected a single letter

--- main.py
+++ main.py
@@ -9,13 +9,15 @@ def run():
     import cv2 as OpenCV
     import mouse
 
+    import time
+
     user32 = windll.user32
     user32.SetProcessDPIAware()
 
     def PIL_to_OpenCV(img):
         numpy_img = numpy.array(img)
-        #imshow("Numpy First", numpy_img)
-        return numpy_img
+        cv_image = OpenCV.cvtColor(numpy_img, OpenCV.COLOR_RGB2BGR)
+        return cv_image
 
     def getHandleFromTitle(t, exact=False):
         toplist, windowList = [], []
@@ -31,24 +33,33 @@ def run():
 
     def screenshotWindow(hwnd):
         win32gui.SetForegroundWindow(hwnd)
+        time.sleep(0.5)
         boundingBox = win32gui.GetWindowRect(hwnd)
         print("Bounding box of window:\t"+str(boundingBox))
         img = ImageGrab.grab(boundingBox)
 
         return img
     
-    handle = getHandleFromTitle("avast")[0][0]
-    image = screenshotWindow(handle)
-    #image.show()
+    world = OpenCV.imread("assets/tempScreen.png", 0)
+    d = OpenCV.imread("assets/D.png", 0)
 
-    OpenCV.imshow("TITLE",PIL_to_OpenCV(image))
-    OpenCV.waitKey(0)
-    OpenCV.imwrite("assets/tempScreen.png")
-    OpenCV.destroyAllWindows()
+    height, width = d.shape
+
+    methods = [OpenCV.TM_CCOEFF,OpenCV.TM_CCOEFF_NORMED,OpenCV.TM_CCORR,OpenCV.TM_CCOEFF_NORMED,OpenCV.TM_SQDIFF,OpenCV.TM_SQDIFF_NORMED]
+
+    for method in methods:        
+        world2 = world.copy()
+
+        result = OpenCV.matchTemplate(world2, d, method)
+        min_value, max_value, min_location, max_location = OpenCV.minMaxLoc(result)
+        print(max_value,max_location)
         
-    mouse.move(100,100,duration=0.5)
-
-
+        location = max_location
+        bottom_right = (location[0]+width,location[1]+height)
+        OpenCV.rectangle(world2, location, bottom_right, 255, 5)
+        OpenCV.imshow("Match", world2)
+        OpenCV.waitKey(0)
+        OpenCV.destroyAllWindows()
 
 
 
